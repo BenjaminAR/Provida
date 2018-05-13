828f62092fca7d38c57a2affe68cdb18e58af437
benjaminar <developerbenjaminar@gmail.com>
2018-05-13 16:55:32 -0500

    comienzos de mi pagina web

---
+++ app.js
@@ -0,0 +1,9 @@
+function sumar(){
+    var x = parseInt(document.getElementById('i3').value);
+    var y = parseInt(document.getElementById('i1').value);
+    var a = parseInt(document.getElementById('i4').value);
+    var b = parseInt(document.getElementById('i2').value);
+    var res1;
+    document.getElementById('el-res').innerHTML = res1= x*y;
+    document.getElementById('el-res1').innerHTML = res1/y;
+}   
